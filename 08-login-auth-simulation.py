1c20f2ceccb67ddbc070d71476e21ec8a05aaf33
lalith-sai-charan-9 <lalithsaicharan9@gmail.com>
2025-05-04 12:30:57 +0530

    challenge 8 completed

---
+++ 08-login-auth-simulation.py
@@ -0,0 +1,8 @@
+userName = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if userName == "admin" and password == "1234":
+    print("Login successful!")
+else:
+    print("Login failed!")
+
