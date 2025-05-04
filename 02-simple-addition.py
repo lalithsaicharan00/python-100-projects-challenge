6e5bf623d515f66ba53514ea34d67b031a298d99
lalith-sai-charan-9 <lalithsaicharan9@gmail.com>
2025-05-04 08:30:04 +0530

    challenge 2 completed

---
+++ 02-simple-addition.py
@@ -0,0 +1,5 @@
+firstNumber = int(input("Enter first number: "))
+secondNumber = int(input("Enter second number: "))
+
+sum = firstNumber + secondNumber
+print(f"The sum of {firstNumber} and {secondNumber} is {sum}.")
