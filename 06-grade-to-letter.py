6fed36077845b112bd75d4cb4394556543575cae
lalith-sai-charan-9 <lalithsaicharan9@gmail.com>
2025-05-04 12:18:45 +0530

    challenge 6 completed

---
+++ 06-grade-to-letter.py
@@ -0,0 +1,15 @@
+grade = int(input("Enter your grade: "))
+
+if grade >= 90:
+    print("grade A")
+elif grade >= 80:
+    print("grade B")
+elif grade >= 70:
+    print("grade C")
+elif grade >= 60:
+    print("grade D")
+elif grade >= 50:
+    print("grade E")
+else:
+    print("fail")
+
